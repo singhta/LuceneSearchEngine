e0e8a0c129ef3b4154f6223c694da05612c56ff5
taran <taranhundal@ymail.com>
2020-02-28 15:08:47 +0000

    final commit

---
+++ src/main/java/com/Taran/Singh/CranQuery.java
@@ -0,0 +1,25 @@
+package com.Taran.Singh;
+
+public class CranQuery {
+
+
+    private String id;
+    private String abstr;
+
+    public CranQuery(String id, String abstr) {
+        this.id = id;
+        this.abstr = abstr;
+    }
+
+
+    public String getID() {
+        return id;
+    }
+
+
+    public String getAbstr() {
+        return abstr;
+    }
+
+
+}
